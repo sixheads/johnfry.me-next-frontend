93b2568f63642e4a4a0be595e6db586f41d04676
John Fry <john@sixheads.com>
2022-07-26 18:10:25 +1000

    Tweak transition settings

--- components/Transition.js
+++ components/Transition.js
@@ -9,15 +9,15 @@ const Transition = ({ children }) => {
       opacity: 0,
       y: 40,
       transition: {
-        duration: 0.75,
+        duration: 0.5,
       },
     },
     in: {
       opacity: 1,
       y: 0,
       transition: {
-        duration: 0.75,
-        delay: 0.5,
+        duration: 0.5,
+        delay: 0.25,
       },
     },
   };
