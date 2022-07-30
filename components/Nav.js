3a896d3ba18f513a31665f2271c26232c57af386
John Fry <john@sixheads.com>
2022-07-31 07:55:39 +1000

    Update nav to flex and tweak styles

--- components/Nav.js
+++ components/Nav.js
@@ -1,39 +1,50 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import styles from '../styles/Nav.module.scss'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from '../styles/Nav.module.scss';
 
 export default function Nav() {
-
   const router = useRouter();
   const currentRoute = router.pathname;
 
   console.log(currentRoute);
 
   return (
-    <nav className={`${styles.nav} site-nav`}>
+    <nav className={`${styles.nav} site-nav`} role="navigation">
       <ul>
         <li>
-          <Link href="/" >
-            <a className={currentRoute === "/" ? "active" : "non-active"}>
+          <Link href="/">
+            <a
+              className={
+                currentRoute === '/' ? 'active' : 'non-active'
+              }
+            >
               Work
             </a>
           </Link>
         </li>
         <li>
-          <Link href="/about" >
-            <a className={currentRoute === "/about" ? "active" : "non-active"}>
+          <Link href="/about">
+            <a
+              className={
+                currentRoute === '/about' ? 'active' : 'non-active'
+              }
+            >
               About
             </a>
           </Link>
         </li>
         <li>
-          <Link href="/contact" >
-            <a className={currentRoute === "/contact" ? "active" : "non-active"}>
+          <Link href="/contact">
+            <a
+              className={
+                currentRoute === '/contact' ? 'active' : 'non-active'
+              }
+            >
               Contact
             </a>
           </Link>
         </li>
       </ul>
     </nav>
-  )
+  );
 }
