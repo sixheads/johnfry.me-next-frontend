9297fa79cd6b3c6eefd78cefa9f1caa4fffef5cf
John Fry <john@sixheads.com>
2022-07-30 15:39:28 +1000

    Move work styles to own partial

--- components/Work.js
+++ components/Work.js
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import styles from '../styles/Work.module.scss';
 
 export default function Work({ project }) {
   const { title, projectLink } = project.attributes;
