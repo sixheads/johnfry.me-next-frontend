01b8fc2c7abe92052fe966cf15be5c8174ed15e6
John Fry <john@sixheads.com>
2022-07-31 09:59:39 +1000

    Fix page transition bug

--- components/Transition.js
+++ components/Transition.js
@@ -7,14 +7,14 @@ const Transition = ({ children }) => {
   const variants = {
     out: {
       opacity: 0,
-      x: 100,
+      y: 40,
       transition: {
         duration: 0.4,
       },
     },
     in: {
       opacity: 1,
-      x: 0,
+      y: 0,
       transition: {
         duration: 0.4,
         // delay: 0.25,
