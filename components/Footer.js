342aef54e1388b32ee44f64794f937c85973cd02
John Fry <john@sixheads.com>
2022-07-30 18:09:53 +1000

    Fix footer content on mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,14 @@
-import styles from '../styles/Footer.module.scss'
+import styles from '../styles/Footer.module.scss';
 
 export default function Footer() {
   return (
     <footer className={`${styles.footer} site-footer`}>
-      <p className='copyright small'>&copy; John Fry. All Rights Reserved {new Date().getFullYear()}. </p>
-      <p className='email small'>
+      <p className="copyright small">
+        &copy; {new Date().getFullYear()} John Fry.
+      </p>
+      <p className="email small">
         <a href="mailto:hello@johnfry.me">hello@johnfry.me</a>
       </p>
     </footer>
-  )
+  );
 }
