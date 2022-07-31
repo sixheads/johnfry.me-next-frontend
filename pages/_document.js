9c68f0ebd7247b773ecabbfadfaf831680e147ae
John Fry <john@sixheads.com>
2022-07-31 11:24:37 +1000

    Add id to script element

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ export default function Document() {
         />
 
         <Script
+          id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
